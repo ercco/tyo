ccf0774bdf3d3d759f6414bf2f16413fe38c640c
ercco <32196171+ercco@users.noreply.github.com>
2020-07-21 13:51:13 +0300

    file write, start, stop functions

--- tyo.py
+++ tyo.py
@@ -1,7 +1,23 @@
 import datetime
 
-def get_formatted_time():
-    return datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+def get_formatted_time(datetime_object):
+    return datetime_object.strftime('%Y-%m-%dT%H:%M:%S')
 
 def get_time_object(formatted_time):
     return datetime.datetime.strptime(formatted_time,'%Y-%m-%dT%H:%M:%S')
+
+def write_to_file(datetime_object,event_type):
+    formatted_time = get_formatted_time(datetime_object)
+    with open(formatted_time[0:7]+'.txt','a') as f:
+        if event_type == 'start':
+            f.write('A\t'+formatted_time+'\n')
+        elif event_type == 'stop':
+            f.write('O\t'+formatted_time+'\n')
+
+def start():
+    start_time = datetime.datetime.now()
+    write_to_file(start_time,'start')
+
+def stop():
+    stop_time = datetime.datetime.now()
+    write_to_file(stop_time,'stop')
